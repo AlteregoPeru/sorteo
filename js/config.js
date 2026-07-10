1b06894cb12f817edecd29a1b7038c6fe7b6cc5b
AlteregoPeru <alter.egg17@gmail.com>
2026-07-10 16:42:30 -0500

    Delete js directory

--- js/config.js
+++
@@ -1,8 +0,0 @@
-export const CONFIG = {
-  SUPABASE_URL: 'https://zlfmyecnetadjugfsyrq.supabase.co',
-  SUPABASE_ANON_KEY: 'sb_publishable_dBIdaQzuRvcqhiKdPDHiHg_w4UgXiqK',
-  INSTAGRAM_URL: 'https://www.instagram.com/alteregoperu/?hl=es',
-  COMPANY_NAME: 'ALTEREGO',
-  DRAW_DATE_TEXT: 'Próximamente',
-  REDIRECT_SECONDS: 5
-};
